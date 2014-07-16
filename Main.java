2ea7237951f8a72549fbd51fe2c17e3c99ad387c
Matt Heitzer <mheitzer44@gmail.com>
2014-07-15 21:19:43 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,45 @@
+/**
+ * A DECK OF CARDS
+ * 
+ * This is the main class for my deck of cards implementation.
+ * This is where I can create and use a deck of cards.
+ * 
+ * A deck of cards is made up of 52 or 54 cards, depending if Jokers are included or not.
+ * A deck can be shuffled, dealt, and/or viewed in order.
+ * 
+ * @author Matt Heitzer | mheitzer44@gmail.com
+ * @date 07.15.2014
+ *
+ */
+
+public class Main {
+
+	/**
+	 * This main method will create and display a new deck of cards, 
+	 * then shuffle the deck three times, and display the shuffled deck.
+	 * @param args Input arguments
+	 */
+	public static void main(String[] args) {
+		
+		Deck deck = new Deck();
+		deck.view();
+		
+		deck.shuffle(); // Shuffle the deck
+		deck.shuffle(); // Shuffle the deck
+		deck.shuffle(); // Shuffle the deck
+		
+		deck.view();
+		
+	/**
+		Card randomCard = deck.pickCard();
+		System.out.println("Hmm... I'm thinking...");
+		try {
+			Thread.sleep(5000);
+		} 
+		catch (InterruptedException ie) {
+			System.out.println(ie);
+		}
+		deck.magicTrick(randomCard);
+	*/
+	}
+}
